acccd6d3f267e93a64bf79cdf52d81b73467de6d
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-08-09 16:36:17 +0300

    Creating a User Model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+const User = sequelize.define('user',{
+    id:{
+        primaryKey: true,
+        type: Sequelize.STRING,
+        autoIncrement:true,
+        allowNull:false,
+
+    },
+    name:{
+        type:Sequelize.STRING,
+        allowNull: false,
+
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+module.exports = User;
